14e6112756f211cda40a4037d4a0d53293c91e9c
Nico van Biljon <nico.vbiljon@gmail.com>
2025-01-11 09:59:46 +0200

    Added / learned about arrow functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -32,6 +32,7 @@ console.log(fruitProcessor(5, 0)); // log the result directly to the console
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice); */
 
+/*
 // Function declaration
 function calcAge1(birthYear) {
     return 2037 - birthYear;
@@ -48,3 +49,20 @@ const age2 = calcAge2(1991);
 
 console.log(age1);
 console.log(age2);
+*/
+
+// Arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    //return retirement;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, "Nico"));
+console.log(yearsUntilRetirement(1980, "Piet"));
