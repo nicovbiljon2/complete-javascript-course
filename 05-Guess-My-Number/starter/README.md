7f61f1599647903b8e602b6e80da2e208c667b66
Nico van Biljon <nico.vbiljon@gmail.com>
2025-02-10 18:45:13 +0200

    Guess-My-Number Project

---
+++ 05-Guess-My-Number/starter/README.md
@@ -0,0 +1,3 @@
+Project: Building a Guess my Number
+
+Added querySelector to select specific class elements from the DOM and assign new value to the .message and .number element.
--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,25 @@
-'use strict';
+"use strict";
+
+/*
+// Reading the content of class element .message from the DOM
+console.log(document.querySelector(".message").textContent);
+// Changing the text content of the DOM class element .message
+document.querySelector(".message").textContent = "🎊 Correct Number!";
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 10;
+
+document.querySelector(".guess").value = 23;
+console.log(document.querySelector(".guess").value);
+*/
+//Starting of the project
+// add functionality to the button
+document.querySelector(".check").addEventListener("click", function () {
+    const guess = Number(document.querySelector(".guess").value);
+    console.log(guess, typeof guess);
+
+    // implement game logic: check if there is a value, if not prompt the user to add a value
+    if (!guess) {
+        document.querySelector(".message").textContent = "⛔ No number!";
+    }
+});
