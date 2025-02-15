56e98640b02e7e66707327ec4307e1c5b115877e
Nico van Biljon <nico.vbiljon@gmail.com>
2025-02-15 09:32:35 +0200

    add functionality to the game - rolling the dice and update README.md file

--- 07-Pig-Game/starter/README.md
+++ 07-Pig-Game/starter/README.md
@@ -2,6 +2,6 @@ Project 3: Pig Game
 
 It is a code a long practice project building a game.
 How the game works is that player one roll the dice and the number rolled gets added to current score for that round. This is the same for player two.
-Both player can select hold score which adds the value to the total score. The we switch players.
-If one of the players roll a 1 they looses all their current score and their current score goes back to zero. Total scores stays the same.
+Both player can select hold score at any tome to add the current score to the total score. Then the players also switch turns.
+If players one rolls 1 the player looses all their points and their current score goes back to zero. Then it is the other player turn. Total scores stays the same.
 The first player who reaches a score of 100 wins.
--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,11 +1,36 @@
-'use strict';
+"use strict";
 
-// Selecting elements
-const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('score--1');
-const diceEl = document.querySelector('.dice');
+// Selecting the elements
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+const diceEl = document.querySelector(".dice");
+const btnNewEL = document.querySelector(".btn--new");
+const btnRollEL = document.querySelector(".btn--roll");
+const btnHoldEL = document.querySelector(".btn--hold");
 
-// Starting conditions
-score0El.textContent = 0;
+// Starting the conditions
+score0El.textContent = 0; // set the text content to zero
 score1El.textContent = 0;
-diceEl.classList.add('hidden');
+diceEl.classList.add("hidden");
+
+let currentScore = 0;
+
+// Implementing functionality to the game - rolling the dice
+btnRollEL.addEventListener("click", function () {
+    // 1. Generate a random dice rolled number
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
+    // 2. Display the dice
+    diceEl.classList.remove("hidden");
+    diceEl.src = `dice-${dice}.png`;
+    // 3. Check if dice rolled is equal to  1
+    if (dice !== 1) {
+        // add dice number to current score
+        currentScore += dice;
+        current0El.textContent = currentScore; // change later
+    } else {
+        // if true, switch to next player
+    }
+});
