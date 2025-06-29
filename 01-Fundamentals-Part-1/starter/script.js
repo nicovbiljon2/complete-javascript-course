f22065fe379d40f358845d13150f1458f7015f49
Nico van Biljon <nico.vbiljon@gmail.com>
2025-06-29 11:15:40 +0200

    Learned about Let, const and var

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -18,7 +18,7 @@ let mySecondJob = "coder";
 console.log(myFirstJob + " " + mySecondJob);*/
 
 // Data Types
-let javascriptIsFun = true;
+/*let javascriptIsFun = true;
 console.log(javascriptIsFun);
 
 // console.log(typeof true);
@@ -34,4 +34,18 @@ console.log(year);
 console.log(typeof year);
 
 year = 1991;
-console.log(typeof year);
+console.log(typeof year);*/
+
+// let, Const and Var
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990; - will get an error can't change a const variable
+// const job; - will get an error missing initializer
+
+var job = "programmer";
+job = "coder";
+
+lastName = "van Biljon";
+console.log(lastName);
