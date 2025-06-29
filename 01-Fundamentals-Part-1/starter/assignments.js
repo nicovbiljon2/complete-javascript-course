e9cbe50c32032589c1be654ade03264de68fb5f9
Nico van Biljon <nico.vbiljon@gmail.com>
2025-06-29 10:30:47 +0200

    Added and learned about variables and data types
    added a codingExercises.js and assignments.js file for the assignments and challenges to practice what I have learned and linked it in the HTML file

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,13 @@
+/* 
+Assignments 1:
+Values and Variables
+1. Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+2. Log their values to the console.*/
+
+let country = "South Africa";
+let continent = "Africa";
+let population = "64.75 million";
+
+console.log(country);
+console.log(continent);
+console.log(population);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,37 @@
-let js = "amazing";
-        if (js === "amazing") alert("JavaScript is FUN!");
+/*let js = "amazing";
+    if (js === "amazing") alert("JavaScript is FUN!");
+console.log(40 + 8 + 23 - 10);
+console.log("Nico");
+console.log(23);*/
 
-        console.log(40 + 8 + 23 - 10);
-        
+// Variables names
+/*let firstName = "Matilda";
+let PI = 3.1415;
+
+console.log(firstName);
+console.log(firstName);
+console.log(PI);
+
+let myFirstJob = "programmer";
+let mySecondJob = "coder";
+
+console.log(myFirstJob + " " + mySecondJob);*/
+
+// Data Types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+console.log(typeof "Nico");
+
+javascriptIsFun = "YES!";
+console.log(javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
