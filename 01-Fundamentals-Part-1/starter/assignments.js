9d5f2073ba85ba766ff65ad1d2c4ab3f872c5995
Nico van Biljon <nico.vbiljon@gmail.com>
2025-06-29 10:37:07 +0200

    Update assignments.js

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,6 +1,5 @@
-/* 
-Assignments 1:
-Values and Variables
+
+/* Values and Variables
 1. Declare variables called country, continent and population and assign their values according to your own country (population in millions).
 2. Log their values to the console.*/
 
@@ -8,6 +7,14 @@ let country = "South Africa";
 let continent = "Africa";
 let population = "64.75 million";
 
-console.log(country);
-console.log(continent);
-console.log(population);
+/* Data Types
+1. Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet.
+2. Log the types of isIsland, population, country and language to the console.
+*/
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log( typeof country);
+console.log(typeof language);
