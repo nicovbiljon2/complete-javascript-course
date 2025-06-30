b71ef65fc0b17a6fe7bec710e20b11633853ebd7
Nico van Biljon <nico.vbiljon@gmail.com>
2025-06-30 19:01:08 +0200

    added coding exercise challenge number 1

--- 01-Fundamentals-Part-1/starter/codingExercises.js
+++ 01-Fundamentals-Part-1/starter/codingExercises.js
@@ -1,1 +1,47 @@
+/*CHALLENGE #1
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / (height * height) (mass in kg and height in meters).
 
+Your task is to write some code to help them:
+1. Store Mark's and John's mass and height in variables called massMark, heightMark, massJohn and heightJohn.
+2. Calculate both their BMIs using the formula, and store the results in two variables called BMIMark and BMIJohn.
+3. Log the value of BMIMark and BMIJohn to the console.
+4. BONUS: Create a boolean variable markHigherBMI containing information about whether Mark has a higher BMI than John. Log it to the console too
+
+TEST DATA 1: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 m tall.
+
+TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+
+👋 OPTIONAL: You can watch my solution in video format in the next lecture
+
+IMPORTANT: Please make sure to use exactly this formula mass / (height * height), and not this one mass / (height ** 2).
+*/
+
+// Test Data 1
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+console.log(BMIMark);
+console.log(BMIJohn);
+
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(markHigherBMI);
+
+// Test Data 2
+const massMark2 = 95;
+const heightMark2 = 1.88;
+const massJohn2 = 85;
+const heightJohn2 = 1.76;
+
+const BMIMark2 = massMark2 / (heightMark2 * heightMark2);
+const BMIJohn2 = massJohn2 / (heightJohn2 * heightJohn2);
+
+console.log(BMIMark2);
+console.log(BMIJohn2);
+
+const markHigherBMI2 = BMIMark2 > BMIJohn2;
+console.log(markHigherBMI2);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -52,7 +52,7 @@ console.log(lastName);*/
 
 // Basic operators
 // Math operators
-const now = 2037;
+/* const now = 2037;
 const ageNico = now - 1974;
 const ageSarah = now - 2020;
 console.log(ageNico, ageSarah);
@@ -80,6 +80,17 @@ console.log(ageSarah >= 18);
 const isFullAge = ageSarah >= 18;
 console.log(isFullAge);
 
-console.log(now - 1991 > now - 2018);
+console.log(now - 1991 > now - 2018);*/
 
+// Operator precedence
+const now = 2037;
+const ageNico = now - 1974;
+const ageSarah = now - 2018;
+console.log(now - 1974 > now - 2018);
 
+let x, y;
+x = y = 25 -10 - 5; // x = y = 10, x = 10 
+console.log(x, y);
+
+const averageAge = (ageNico + ageSarah) / 2;
+console.log( ageNico, ageSarah, averageAge);
