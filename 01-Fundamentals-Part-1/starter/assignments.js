bb02fb223e462f4b99c66d65864f8af12630371b
Nico van Biljon <nico.vbiljon@gmail.com>
2025-06-30 18:25:24 +0200

    added and learned about basic operators

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -29,7 +29,7 @@ console.log(typeof language);*/
 
 Try to change one of the changed variables now, and observe what happens.*/
 
-language = "Afrikaans";
+/* language = "Afrikaans";
 const country = "South Africa";
 const continent = "Africa";
 let population = "64.75 million";
@@ -40,4 +40,30 @@ console.log(language);
 console.log(country);
 console.log(continent);
 console.log(population);
-console.log(isIsland);
+console.log(isIsland);*/
+
+/* Basic Operators
+1. If your country split in half, and each half would contain half the population, then how many people would live in each half?
+2. Increase the population of your country by 1 and log the result to the console.
+3. Finland has a population of 6 million. Does your country have more people than Finland?
+4. The average population of a country is 33 million people. Does you country have less people than the average country?
+5. Based on the variables you created, create a new variable description which contains a string with this format: 'Portugal is in Europe, and its 11 million people speak portuguese'.
+*/
+const country = "South Africa";
+const continent = "Africa";
+language = "Afrikaans"
+let population = 64.75; // this is in millions
+
+console.log(population / 2);
+
+population++;
+console.log(population);
+
+const populationFinland = 6; // this is in millions
+console.log(population > populationFinland);
+
+const averagePopulation = 33; // this is in millions
+console.log(population < averagePopulation);
+
+const description = country + " is in " + continent + " , and its " + population + " million people speak " + language;
+console.log(description);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -37,7 +37,7 @@ year = 1991;
 console.log(typeof year);*/
 
 // let, Const and Var
-let age = 30;
+/*let age = 30;
 age = 31;
 
 const birthYear = 1991;
@@ -48,4 +48,38 @@ var job = "programmer";
 job = "coder";
 
 lastName = "van Biljon";
-console.log(lastName);
+console.log(lastName);*/
+
+// Basic operators
+// Math operators
+const now = 2037;
+const ageNico = now - 1974;
+const ageSarah = now - 2020;
+console.log(ageNico, ageSarah);
+
+console.log(ageNico * 2, ageNico / 10, ageSarah / 2, 2 **3);
+// 2 ** 3 2 to the power of 3 = 2 * 2 * 2 
+
+const firstName = "Nico";
+const lastName = "van Biljon";
+console.log(firstName + " " +  lastName);
+
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 = 101
+x--; // x = x - 1 = 100
+x--; // x = x -1 = 99
+console.log(x);
+
+// Comparison operators
+console.log(ageNico > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+console.log(isFullAge);
+
+console.log(now - 1991 > now - 2018);
+
+
