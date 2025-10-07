8eb9df678ea129983d67dbbe7a7c145f926c241b
Nico van Biljon <141059907+nicovbiljon2@users.noreply.github.com>
2025-10-07 19:16:26 +0200

    Linked a script.js file and  assignments.js file
    Learned about values and how to create variables

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,12 @@
+/*Values and Variables﻿
+Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+Log their values to the console.*/
+
+let country = "South Africa";
+let continent = "Africa";
+let population = "64";
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,21 @@
+let js = "amazing";
+// if (js === "amazing") alert("JavaSript is FUN!");
+
+console.log(40 + 8 + 23 - 10);
+
+// Values and Variables
+console.log("Nico");
+console.log(23);
+
+let firstName = "Matilda";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "Programmer";
+let job2 = "Teacher";
+
+console.log(myFirstJob);
