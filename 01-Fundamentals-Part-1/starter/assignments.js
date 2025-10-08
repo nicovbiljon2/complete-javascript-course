455bacd72b70ae6bb5b8aa7a61d75956f6bf5083
Nico van Biljon <141059907+nicovbiljon2@users.noreply.github.com>
2025-10-08 19:18:07 +0200

    Learned about data types and basic operators

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,12 +1,42 @@
 /*Values and Variables﻿
 Declare variables called country, continent and population and assign their values according to your own country (population in millions).
 
-Log their values to the console.*/
+Log their values to the console.
+*/
 
-let country = "South Africa";
-let continent = "Africa";
-let population = "64";
+const country = "South Africa";
+const continent = "Africa";
+let population = 64;
 
 console.log(country);
 console.log(continent);
 console.log(population);
+
+
+/*Data Types﻿
+Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet.
+
+Log the types of isIsland, population, country and language to the console.
+*/
+
+let isIsland = true;
+let language;
+
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
+
+
+/*let, const and var﻿
+Set the value of language to the language spoken where you live (some countries have multiple languages, but just choose one).
+
+Think about which variables should be const variables (which values will never change, and which might change?). Then, change these variables to const.
+
+Try to change one of the changed variables now, and observe what happens.
+ */
+
+language = "English";
+// country = "Italy"
+console.log(language);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,9 +1,9 @@
-let js = "amazing";
+/*let js = "amazing";
 // if (js === "amazing") alert("JavaSript is FUN!");
 
 console.log(40 + 8 + 23 - 10);
 
-// Values and Variables
+// Values and Variables name conventions
 console.log("Nico");
 console.log(23);
 
@@ -19,3 +19,72 @@ let job1 = "Programmer";
 let job2 = "Teacher";
 
 console.log(myFirstJob);
+
+
+// Data types 
+let javaSriptIsFun = true;
+console.log(javaSriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javaSriptIsFunavaSriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Nico");
+
+javaSriptIsFun = "YES!";
+console.log(typeof javaSriptIsFun);
+
+// empty vairalbe - undifined 
+let year;
+console.log(year);
+console.log(typeof year);
+
+// assign a new value to year
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+// Declare a variable 
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = "programmer";
+job = "teacher";
+*/
+
+// Basic operators
+// Math operators 
+const now = 2037
+
+const ageNico = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageNico, ageSarah);
+
+console.log(ageNico * 2, ageNico / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "Nico";
+const lastName = "van Biljon";
+console.log(firstName + " " + lastName);
+
+// Assigment operators 
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 = 101
+x--; // x = x - 1 = 100
+x--; // x = x - 1 = 99
+console.log(x);
+
+// Comparison oprators 
+console.log(ageNico > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
