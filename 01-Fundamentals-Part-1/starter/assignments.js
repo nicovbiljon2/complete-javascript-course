a83e87b4d2800880e9738102a9b4940edf238975
Nico van Biljon <141059907+nicovbiljon2@users.noreply.github.com>
2025-10-09 18:48:12 +0200

    Learned about operator precedence and completed basic operator assignment

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -40,3 +40,23 @@ Try to change one of the changed variables now, and observe what happens.
 language = "English";
 // country = "Italy"
 console.log(language);
+
+/*Basic Operators﻿
+If your country split in half, and each half would contain half the population, then how many people would live in each half?
+Increase the population of your country by 1 and log the result to the console.
+Finland has a population of 6 million. Does your country have more people than Finland?
+The average population of a country is 33 million people. Does you country have less people than the average country?
+Based on the variables you created, create a new variable description which contains a string with this format: 'Portugal is in Europe, and its 11 million people speak portuguese'.
+ */
+
+console.log(population / 2);
+
+population += 1;
+
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+
+description1 = country + " is in " + continent + ", and its " + population + " million people speak " + language;
+
+console.log(description1)
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -55,7 +55,7 @@ const birthYear = 1991;
 
 var job = "programmer";
 job = "teacher";
-*/
+
 
 // Basic operators
 // Math operators 
@@ -88,3 +88,18 @@ console.log(ageSarah >= 18);
 const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2018);
+*/
+
+const now = 2037;
+const ageNico = now - 1991;
+const ageSarah = now - 2018;
+console.log(now - 1991 > now - 2018);
+
+console.log(25 - 10 -5);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const avarageAge = (ageNico + ageSarah) / 2;
+console.log(ageNico, ageSarah, avarageAge);
