d865d6d3ec662b97d0c9d17c0e2df650b47790d1
Nico van Biljon <141059907+nicovbiljon2@users.noreply.github.com>
2025-10-16 19:08:27 +0200

    Started Fundamentals Part 2
    Learned how to active strict mode in JavaScript and how it can help us to write more secure code and make bugs in our code visible in the console.

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,11 @@
+"use strict"; // activating strict mode 
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log("I can drive 😊");
+
+// const interface = "Audio";
+// const private = 534;
+// const if = 234;
