57c56da223d15728a5f04e0ca8ce7b0622d9446b
Nico van Biljon <141059907+nicovbiljon2@users.noreply.github.com>
2025-10-18 12:11:25 +0200

    Learned about function declaration and function expressions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,6 @@
 "use strict"; // activating strict mode 
 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -9,3 +10,46 @@ if (hasDriversLicense) console.log("I can drive 😊");
 // const interface = "Audio";
 // const private = 534;
 // const if = 234;
+ */
+
+/*Functions 
+function logger() {
+    console.log("My name is Nico");
+}
+
+logger(); // invoking, running, calling the function
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    // console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 3));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+*/
+
+/* Function declarations and expressions */
+// function declaration
+const age1 = calcAge1(1991);
+console.log(age1);
+
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+//const age1 = calcAge1(1991);
+// console.log(age1);
+
+// function expression 
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const age2 = calcAge2(1991);
+console.log(age2);
