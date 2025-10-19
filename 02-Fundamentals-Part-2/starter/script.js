2a371cbeca68423041f561bdd26219730b5eb12e
Nico van Biljon <141059907+nicovbiljon2@users.noreply.github.com>
2025-10-19 10:30:16 +0200

    Learned more about functions
    Arrow functions
    Calling a function inside another functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -35,7 +35,7 @@ const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 */
 
-/* Function declarations and expressions */
+/* Function declarations and expressions 
 // function declaration
 const age1 = calcAge1(1991);
 console.log(age1);
@@ -53,3 +53,65 @@ const calcAge2 = function (birthYear) {
 
 const age2 = calcAge2(1991);
 console.log(age2);
+*/
+
+/* Arrow functions 
+// Function expression 
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+// Arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years.`;
+}
+
+console.log(yearsUntilRetirement(1991, "Nico"));
+console.log(yearsUntilRetirement(1980, "Bob"));
+*/
+
+/* Functions calling other functions 
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const appelPieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${appelPieces} apples and ${orangePieces} oranges.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+*/
+
+/* Reviewing functions */
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if(retirement > 0) {
+        // return retirement;
+        return `${firstName} retires in ${retirement} years`;
+    } else {
+        return `${firstName} has already retired 🎉`;
+        // return -1;
+    }
+
+    // return retirement;
+    // return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, "Nico"));
+console.log(yearsUntilRetirement(1970, "Mike"));
