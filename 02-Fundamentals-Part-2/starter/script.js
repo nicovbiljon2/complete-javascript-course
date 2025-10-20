ee39ff34ad2dcb7ae5f0a937e42260621e397d3b
Nico van Biljon <141059907+nicovbiljon2@users.noreply.github.com>
2025-10-20 18:54:55 +0200

    Linked coding-challenges.js to index.html and learned about arrays
    Completed challenge #1 - functions
    Introduction to Arrays but need to review the lecture again.

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -92,7 +92,7 @@ function fruitProcessor(apples, oranges) {
 console.log(fruitProcessor(2, 3));
 */
 
-/* Reviewing functions */
+/* Reviewing functions 
 const calcAge = function (birthYear) {
     return 2037 - birthYear;
 }
@@ -114,4 +114,42 @@ const yearsUntilRetirement = function (birthYear, firstName) {
 }
 
 console.log(yearsUntilRetirement(1991, "Nico"));
-console.log(yearsUntilRetirement(1970, "Mike"));
+console.log(yearsUntilRetirement(1970, "Mike"));*/ 
+
+/* Introduction to Arrays */
+const friends = ["Michael", "Steven", "Pter"];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020);
+console.log(y);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Jay";
+console.log(friends);
+// friends = ["Bob", "Alice"];
+
+const firstName = "Nico";
+const nico = [firstName, "van Biljon", 2037 - 1990, "teacher", friends];
+
+console.log(nico);
+console.log(nico. length);
+
+// Exercise
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
