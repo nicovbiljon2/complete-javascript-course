168b6e9c912e9932ef2199d25a4daa61565ab6db
Nico van Biljon <141059907+nicovbiljon2@users.noreply.github.com>
2025-11-11 19:11:57 +0200

    Add functionality to two sections
    Holding the score when the hold button is clicked
    Busy with the functionality for resetting the game when the new game button is clicked.
    Also fixed an error on line 13 - didn't add the dot before the class name.

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -10,39 +10,98 @@ const current1El = document.getElementById("current--1");
 const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
-const btnHold = document.querySelector("btn--hold");
-
-const scores = [0, 0]; // empty scores array to store the final score of the players
-let currentScore = 0;
-let activePlayer = 0;
+const btnHold = document.querySelector(".btn--hold");
 
 // Starting conditions
-score0El.textContent = 0; // Setting the score to zero
-score1El.textContent = 0;
-diceEl.classList.add("hidden"); // hide the dice
+const init = function () {
+  const scores = [0, 0]; // empty scores array to store the final score of the players
+  let currentScore = 0;
+  let activePlayer = 0;
+  let playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add("hidden"); // hide the dice
+  player0EL.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0EL.classList.add("player--active");
+  player1El.classList.remove("player--active");
+};
+init();
+
+// Switch players
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0; // set the text content of the active player score back to zero
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0; // set current score to zero
+  player0EL.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
 
 // Rolling dice functionality
 btnRoll.addEventListener("click", function () {
-  // 1. Generating a random dice roll
-  const dice = Math.trunc(Math.random() * 6 + 1);
-  console.log(dice);
+  if (playing) {
+    // 1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6 + 1);
+    console.log(dice);
 
-  // 2. Display dice
-  diceEl.classList.remove("hidden");
-  diceEl.src = `dice-${dice}.png`;
+    // 2. Display dice
+    diceEl.classList.remove("hidden");
+    diceEl.src = `dice-${dice}.png`;
 
-  // 3. Check for rolled 1
-  if (dice !== 1) {
-    // Add dice to the current score
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    // Switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0; // set the text content of the active player score back to zero
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    currentScore = 0; // set current score to zero
-    player0EL.classList.toggle("player--active");
-    player1El.classList.toggle("player--active");
+    // 3. Check for rolled 1
+    if (dice !== 1) {
+      // Add dice to the current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Switch to next player
+      switchPlayer();
+    }
   }
 });
+
+btnHold.addEventListener("click", function () {
+  if (playing) {
+    // 1. Add current score to the active player's score
+    scores[activePlayer] += currentScore;
+    // score[1] = score[1] + currentScore
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // 2. Check if player's score is >= 100
+    if (scores[activePlayer] >= 20) {
+      // If yes finsih the game
+      playing = false;
+      diceEl.classList.add("hidden");
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+    } else {
+      // If no switch to the next player
+      switchPlayer();
+    }
+  }
+});
+
+// Challenge - add functionality to the new game button
+// My solution
+// btnNew.addEventListener("click", function () {
+//   score0El.textContent = 0;
+//   score1El.textContent = 0;
+//   current0El.textContent = 0;
+//   current1El.textContent = 0;
+//   player0EL.classList.remove("player--winner");
+//   player1El.classList.remove("player--winner");
+//   player0EL.classList.remove("player--active");
+//   player1El.classList.remove("player--active");
+// });
+
+// Correct Solution
