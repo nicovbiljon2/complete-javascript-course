f804d29483815eaa663df544cbadb51e6d2054c9
Nico van Biljon <141059907+nicovbiljon2@users.noreply.github.com>
2026-06-06 11:38:04 +0200

    Link script.js file
    Learned about values and variables

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,15 @@
+let js = "amazing";
+//if (js === "amazing") alert("JavaScript is FUN!");
+console.log(40 + 8 + 23 - 10);
+
+console.log("Nico");
+console.log(23);
+
+let firstName = "Matilda";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = "Coder";
+let myCurrentJob = "teacher";
+console.log(myFirstJob);
